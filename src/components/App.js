abf40a2a2027e7c52268d7a4ec20a8e7ec34b3a5
NADEEM  ABBAS <79992599+nadeemabbas24@users.noreply.github.com>
2023-05-10 17:33:53 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,8 +6,13 @@ const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Enter Your Name:</p>
+        <input id="input">
+        <p className="greet"></P>
     </div>
   )
+  
+   
 }
 
 export default App
